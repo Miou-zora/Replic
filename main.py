828436690f5df2fd413c5d4860008b363a643a51
lhay <lylian.hay@epitech.eu>
2023-03-11 20:46:43 +0100

    feat(flag -f): add the flag -friends for invite all friends for project

--- main.py
+++ main.py
@@ -39,7 +39,6 @@ def generate_mirror(orga_name: str, repo_name: str, github: Github):
             has_wiki=False,
             private=True,
         )
-    repo = user.get_repo(mirror_name)
     f = open(os.path.expanduser('~') + "/.ssh/id_rsa", "r")
     private_key = f.read()
     f.close()
@@ -85,19 +84,32 @@ def push_mirror(mirror_name: str, binary_name: str):
             print(action[1])
             exit(84)
 
-def main():
-    if len(argv) != 2:
-        exit(84)
-    repo_info = argv[1].split(":")[1].split("/")
-    json_file = json.load(open("data.json"))
+def add_collaborators(github_usernames: list, repo_name: str, github: Github):
+    user = github.get_user()
+    repo = user.get_repo(repo_name)
+    for github_username in github_usernames:
+        repo.add_to_collaborators(github_username[0], permission="push")
 
+def main():
+    if len(argv) <= 2:
+        exit(84)
+    github_usernames = []
+    i = 1
+    while i < len(argv):
+        arg = argv[i]
+        if arg == "--friend":
+            github_usernames.append(argv[i + 1:i + 3])
+            i += 7
+    repo_info = argv[len(argv) - 1].split(":")[1].split("/")
+    json_file = json.load(open("data.json"))
     github_identifier: Github = Github(json_file["token"])
     orga_name = repo_info[0]
     repo_name = ".".join(repo_info[1].split(".")[:-1])
     generate_mirror(orga_name, repo_name, github_identifier)
-    generate_folders_with_repo(argv[1], github_identifier.get_user().login, repo_name)
-    generate_mirror_workflow(argv[1].split('-')[-2], argv[1], repo_name)
-    push_mirror(f"{repo_name}-mirror", argv[1].split('-')[-2])
+    generate_folders_with_repo(argv[len(argv) - 1], github_identifier.get_user().login, repo_name)
+    generate_mirror_workflow(argv[len(argv) - 1].split('-')[-2], argv[len(argv) - 1], repo_name)
+    add_collaborators(github_usernames, f"{repo_name}-mirror", github_identifier)
+    push_mirror(f"{repo_name}-mirror", argv[len(argv) - 1].split('-')[-2])
 
 if __name__ == '__main__':
     main()
