cded6aeb83b75e5f1ad00480763e202d17d61135
lhay <lylian.hay@epitech.eu>
2023-03-12 18:32:54 +0100

    hotfix(argument_manager): if sshkey it's not here so return error

--- flag_friend/argument_manager.py
+++ flag_friend/argument_manager.py
@@ -11,5 +11,7 @@ import argparse
 def argumentManager():
     parser = argparse.ArgumentParser(description="A beautiful mirror-generator.")
     parser.add_argument("--friend", "-f", nargs=1, action="extend", help="Can add friend to mirror repository")
-    parser.add_argument("sshKey", nargs=1)
+    parser.add_argument("sshKey", nargs="+", help="SSH key(s) to be added to the mirror repository")
+    if not parser.parse_args().sshKey:
+        parser.error("arg > 2 is required")
     return parser.parse_args()
