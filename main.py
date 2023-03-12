5adba81a55ffa9d932aaa609ab8c34c0abc7cb74
lhay <lylian.hay@epitech.eu>
2023-03-12 19:11:06 +0100

    hotfix(args): fix the args for name repo and orga

--- main.py
+++ main.py
@@ -18,16 +18,16 @@ from generate_mirror import *
 
 def main():
     args = argumentManager()
-    repo_info = argv[len(argv) - 1].split(":")[1].split("/")
+    repo_info = args.sshKey[0].split(":")[1].split("/")
     json_file = json.load(open("data.json"))
     github_identifier: Github = Github(json_file["token"])
     orga_name = repo_info[0]
     repo_name = ".".join(repo_info[1].split(".")[:-1])
     generate_mirror(orga_name, repo_name, github_identifier)
-    generate_folders_with_repo(argv[len(argv) - 1], github_identifier.get_user().login, repo_name)
-    generate_mirror_workflow(argv[len(argv) - 1].split('-')[-2], argv[len(argv) - 1], repo_name)
+    generate_folders_with_repo(args.sshKey[0], github_identifier.get_user().login, repo_name)
+    generate_mirror_workflow(args.sshKey[0].split('-')[-2], args.sshKey[0], repo_name)
     add_collaborators(args.friend, f"{repo_name}-mirror", github_identifier)
-    push_mirror(f"{repo_name}-mirror", argv[len(argv) - 1].split('-')[-2])
+    push_mirror(f"{repo_name}-mirror", args.sshKey[0].split('-')[-2])
 
 if __name__ == '__main__':
     main()
