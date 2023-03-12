dc81dad50e29d6faa3c7c2ed18f190681c4f11c9
lhay <lylian.hay@epitech.eu>
2023-03-12 18:40:51 +0100

    hotfix(main): get args.friends

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ from github import Github
 from sys import argv
 import json
 from flag_friend.add_to_collaborators import *
-from flag_friend.add_friend_in_list import *
 from flag_friend.argument_manager import *
 from push_mirror import *
 from generate_mirror_workflow import *
@@ -22,8 +21,6 @@ def main():
         exit(84)
 
     args = argumentManager()
-    github_usernames = []
-    github_usernames = addfriendsinlist(args, github_usernames)
     repo_info = argv[len(argv) - 1].split(":")[1].split("/")
     json_file = json.load(open("data.json"))
     github_identifier: Github = Github(json_file["token"])
@@ -32,7 +29,7 @@ def main():
     generate_mirror(orga_name, repo_name, github_identifier)
     generate_folders_with_repo(argv[len(argv) - 1], github_identifier.get_user().login, repo_name)
     generate_mirror_workflow(argv[len(argv) - 1].split('-')[-2], argv[len(argv) - 1], repo_name)
-    add_collaborators(github_usernames, f"{repo_name}-mirror", github_identifier)
+    add_collaborators(args.friend, f"{repo_name}-mirror", github_identifier)
     push_mirror(f"{repo_name}-mirror", argv[len(argv) - 1].split('-')[-2])
 
 if __name__ == '__main__':
