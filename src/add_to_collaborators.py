a353b1b992be7cc077cd6df2658c8f7793bcef85
Miou-zora <alexandre.franquet@epitech.eu>
2023-03-14 15:53:18 +0100

    refactor(add_to_collaborators.py): add more doc and type variables

--- src/add_to_collaborators.py
+++ src/add_to_collaborators.py
@@ -6,16 +6,26 @@
 ## add_to_collaborators - Add collaborators to the mirror repository on Github
 ##
 
-from github import Github, AuthenticatedUser, Organization, GithubException
+from github import Github, GithubException, AuthenticatedUser, Repository, NamedUser
 
-def add_collaborators(github_usernames, repo_name, github: Github) -> None:
-    user = github.get_user()
-    repo = user.get_repo(repo_name)
+def add_collaborators(github_usernames: list[str], repository_name: str, github: Github) -> None:
+    """Add all of `github_usernames` in repo named `repo_name`
+
+    Args:
+        github_usernames (list[str]): list of github username you want to add
+        repo_name (str): name of the repository where you want to add users
+        github (Github): github key
+
+    Raises:
+        Exception: if username doesn't exist
+    """
+    user: AuthenticatedUser.AuthenticatedUser = github.get_user()
+    repo: Repository.Repository = user.get_repo(repository_name)
     if type(github_usernames) != list:
         return
     for username in github_usernames:
         try:
-            user = github.get_user(username)
+            user: NamedUser.NamedUser = github.get_user(username)
         except GithubException as err:
             raise Exception(f"User {username} does not exist")
         repo.add_to_collaborators(user, "push")
