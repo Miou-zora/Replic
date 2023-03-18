01c1d5bca6d7ae22a5cf238d2bd454dab7960473
miou-zora <alexandre.franquet@epitech.eu>
2023-03-18 23:27:01 +0100

    fix(core): remove blocking commentary and remove sshkey in data.json

--- main.py
+++ main.py
@@ -19,17 +19,17 @@ from src.generate_mirror import *
 
 def main():
     args = argumentManager()
-    # repo_info = args.sshKey[0].split(":")[1].split("/")
-    # json_file = json.load(open("data.json"))
-    # github_identifier: Github = Github(json_file["token"])
-    # orga_name = repo_info[0]
-    # repo_name = ".".join(repo_info[1].split(".")[:-1])
-    # mirror_name = (args.mirror_name[0] if (args.mirror_name != None) else f"{repo_name}-mirror")
-    # generate_mirror(orga_name, repo_name, github_identifier, mirror_name)
-    # generate_folders_with_repo(args.sshKey[0], github_identifier.get_user().login, repo_name, mirror_name)
-    # generate_mirror_workflow(args.sshKey[0].split('-')[-2], args.sshKey[0], repo_name, mirror_name)
-    # add_collaborators(args.friend, mirror_name, github_identifier)
-    # push_mirror(mirror_name, args.sshKey[0].split('-')[-2])
+    repo_info = args.sshKey[0].split(":")[1].split("/")
+    json_file = json.load(open("data.json"))
+    github_identifier: Github = Github(json_file["token"])
+    orga_name = repo_info[0]
+    repo_name = ".".join(repo_info[1].split(".")[:-1])
+    mirror_name = (args.mirror_name[0] if (args.mirror_name != None) else f"{repo_name}-mirror")
+    generate_mirror(orga_name, repo_name, github_identifier, mirror_name)
+    generate_folders_with_repo(args.sshKey[0], github_identifier.get_user().login, repo_name, mirror_name)
+    generate_mirror_workflow(args.sshKey[0].split('-')[-2], args.sshKey[0], repo_name, mirror_name)
+    add_collaborators(args.friend, mirror_name, github_identifier)
+    push_mirror(mirror_name, args.sshKey[0].split('-')[-2])
 
 if __name__ == '__main__':
     main()
