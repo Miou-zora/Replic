d054f8cc7d3d0b214f1ad653d9c90ae7becf9a17
miouzora <alexandre.franquet@epitech.eu>
2023-07-08 23:55:47 +0200

    refactor(CS): make push_mirror compliant with CS

--- src/push_mirror.py
+++ src/push_mirror.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python3
-##
-## @Miou-zora Project, Mirror-Generator, 2023
-## push_mirror.py
-## File description:
-## Push to mirror_name with "CI/CD push" commit name.
-##
+# @Miou-zora Project, Mirror-Generator, 2023
 
 import subprocess
 
+
 def push_mirror(mirror_name: str, project_name: str, commit: str):
-    actions = [(["git", "-C", f"{project_name}/{mirror_name}/", "add", "."], "No add can be done"),
-               (["git", "-C", f"{project_name}/{mirror_name}/", "commit", "-m", commit], "No commit can be done"),
-               (["git", "-C", f"{project_name}/{mirror_name}/", "push"], "No push can be done")]
+    actions = [(["git", "-C", f"{project_name}/{mirror_name}/", "add", "."],
+                "No add can be done"),
+               (["git", "-C", f"{project_name}/{mirror_name}/", "commit",
+                 "-m", commit], "No commit can be done"),
+               (["git", "-C", f"{project_name}/{mirror_name}/", "push"],
+                "No push can be done")]
     for action in actions:
         if subprocess.run(action[0]).returncode == 1:
             raise Exception(action[1])
