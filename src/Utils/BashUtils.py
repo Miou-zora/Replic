49dddb54c158438de6456c781102d8ce0cab1367
miouzora <alexandre.franquet@epitech.eu>
2023-07-09 19:30:31 +0200

    feat(BashUtils): add missing commands

--- src/Utils/BashUtils.py
+++ src/Utils/BashUtils.py
@@ -14,3 +14,24 @@ class BashUtils:
     def mv(src: str, dest: str):
         if subprocess.run(["mv", src, dest]).returncode == 1:
             raise Exception("Can't move folder: " + src)
+
+    class Git:
+        @staticmethod
+        def add(where: str = ".", what: list[str] = ["."]):
+            if subprocess.run(["git", "-C", where, "add", *what]).returncode == 1:
+                raise Exception("No add can be done")
+
+        @staticmethod
+        def push(where: str = "."):
+            if subprocess.run(["git", "-C", where, "push"]).returncode == 1:
+                raise Exception("No push can be done")
+
+        @staticmethod
+        def commit(message: str, where: str = "."):
+            if subprocess.run(["git", "-C", where, "commit", "-m", message]).returncode == 1:
+                raise Exception("No commit can be done")
+
+        @staticmethod
+        def clone(ssh_key: str):
+            if subprocess.run(["git", "clone", ssh_key]).returncode == 1:
+                raise Exception("Can't clone repository: " + ssh_key)
